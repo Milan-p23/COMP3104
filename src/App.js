0bc160c4918f83dd0dca814878cf81fef5ae21d7
Milan Patel <milanapatel23@gmail.com>
2024-10-09 19:57:26 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101397631 || Milan Patel || DevOps</h5>
+      <h5>Developed By: 101397631 || Milan Patel || Devops - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
